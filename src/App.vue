d2c15693fef96cd2789802692a94ba6cadf98f5a
unknown <sinchen@SINCHEN-PC1.lexinfintech.com>
2018-04-05 18:08:46 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+
+export default {
+  name: 'App',
+  components: {
+    MHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
